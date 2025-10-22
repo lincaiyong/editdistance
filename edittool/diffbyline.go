e6c3a71e8355b5d7779e2750a0208f973c91a2a5
lincaiyong <lincaiyong@bytedance.com>
2025-10-22 21:07:41 +0800

    edittool

--- edittool/diffbyline.go
+++ edittool/diffbyline.go
@@ -10,27 +10,39 @@ func DiffByLine(s1, s2 string) string {
 	w2 := strings.Split(s2, "\n")
 	_, ops := editdistance.WordsWithOps(w1, w2)
 	var sb strings.Builder
+
+	var deletions []string
+	var insertions []string
+
+	flushChanges := func() {
+		for _, del := range deletions {
+			sb.WriteString("-|")
+			sb.WriteString(del)
+			sb.WriteString("\n")
+		}
+		for _, ins := range insertions {
+			sb.WriteString("+|")
+			sb.WriteString(ins)
+			sb.WriteString("\n")
+		}
+		deletions = deletions[:0]
+		insertions = insertions[:0]
+	}
 	for _, op := range ops {
 		if op.Type == editdistance.OpKeep {
+			flushChanges()
 			sb.WriteString(" |")
 			sb.WriteString(op.From)
 			sb.WriteString("\n")
 		} else if op.Type == editdistance.OpInsert {
-			sb.WriteString("+|")
-			sb.WriteString(op.To)
-			sb.WriteString("\n")
+			insertions = append(insertions, op.To)
 		} else if op.Type == editdistance.OpDelete {
-			sb.WriteString("-|")
-			sb.WriteString(op.From)
-			sb.WriteString("\n")
+			deletions = append(deletions, op.From)
 		} else {
-			sb.WriteString("-|")
-			sb.WriteString(op.From)
-			sb.WriteString("\n")
-			sb.WriteString("+|")
-			sb.WriteString(op.To)
-			sb.WriteString("\n")
+			deletions = append(deletions, op.From)
+			insertions = append(insertions, op.To)
 		}
 	}
+	flushChanges()
 	return sb.String()
 }
